060dd544b554ccc0d917a92a6a5777727a4994b3
Daniel Hopson <hdhopson88@yahoo.com>
2022-03-04 21:00:37 -0600

    Adds search bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCmBLyviuGH_Gb6haTizFPzuKsD1zBRxVA';
 
 // Create new component. This component should produce
 // some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Place newly created component into DOM
