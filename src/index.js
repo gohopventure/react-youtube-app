22f55231cca81d2492da2daa8c862bb72706697a
Daniel Hopson <hdhopson88@yahoo.com>
2022-03-05 11:37:11 -0600

    Updates app component to be class-based, interfaces w/youtube search api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCmBLyviuGH_Gb6haTizFPzuKsD1zBRxVA';
 
-// Create new component. This component should produce
-// some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component() {
+    constructor(props) {
+        super(props)
+
+        this.state = { videos: [] }; 
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-// Place newly created component into DOM
 module.exports = ReactDOM.render(<App />, document.querySelector('.container'));
